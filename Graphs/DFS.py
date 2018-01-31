1f4a8695b57f5459e1a1ae2d153fcb2fd1967daf
rwiddhi chakraborty <dejavu.chakraborty@gmail.com>
2018-01-31 21:44:36 +0530

    Add files via upload

---
+++ Graphs/DFS.py
@@ -0,0 +1,49 @@
+# Implement Depth First Search
+
+
+def do_DFS(graph, source):
+
+	#Intialise a set for all visited nodes, and a stack for all unvisited notes.
+
+	visited, stack = set(), [source]
+
+	#While stack is not empty
+	while stack:
+		#Pop the last element
+		vertex = stack.pop()
+		#Has this vertex been visitied?
+		if vertex not in visited:
+			visited.add(vertex)
+			stack.extend(graph[vertex] - visited) # Add the next corresponding iterable of source to stack.
+
+	return visited
+
+graph = {'A': set(['B', 'C']),
+         'B': set(['A','D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E'])}
+
+#print do_DFS(graph, 'A')
+
+'''
+Now let's print all possible paths from source to destination
+'''
+
+def paths(graph, source, destination):
+
+	stack = [(source, [source])] #Now our stack holds a tuple of the current vertex and the path
+
+	while stack:
+
+		(vertex, path) = stack.pop()
+
+		for next in graph[vertex] - set(path): # Generator to find next unvisited node
+			if next == destination: # Check if we have reached
+				yield path + [next] 
+			else:
+				stack.append((next, path + [next])) # Else, add the next node to stack, and the value of the next node to path
+
+
+print list(paths(graph, 'A', 'F'))
---
+++ Graphs/TopologicalSort.py
@@ -0,0 +1,70 @@
+'''
+This program implements Topogical Sort. It works like so:
+
+1) Assuming we have a directed acyclic graph G(v, w), find an ordering of (v, w) s.t.
+	v always preceeds w.
+
+2) Create G(v, w) using an adjacency list, and initialise an array of visited vertices.
+3) Intialise queue to store all vertices with in-degree 0.
+4) while queue is not empty, 
+	a) pop queue and decrement in-degrees of all its neighbours by 1.
+	b) Append popped element to visited.
+	c) Enqueue node with in-degree = 0.
+5) Return visited
+NOTE : Graph must be acyclic. One way to check this is if there are no vertices that have in-degree = 0. Return an empty list in such a case.
+'''
+
+
+def topsort(graph):
+
+	# First create a dictionary of indegrees
+	indegrees = {u : 0 for u in graph}
+	for u in graph:
+		for v in graph[u]:
+			indegrees[v] += 1
+
+	#Step 2
+	visited = []
+	queue = [key for key in graph.keys() if indegrees[key] == 0]
+
+	#Step 4
+	while queue:
+		curr_vertex = queue.pop()
+		visited.append(curr_vertex)
+
+		neighbours = graph[curr_vertex] # All neighbours of current vertex
+
+		for key in neighbours: #Iterate over all neighbours of current vertex
+			indegrees[key] -= 1 # Decrement indegree by 1
+
+			if indegrees[key] == 0 and key not in queue: # If new indegree is zero, add to queue
+				queue.insert(0,key)
+
+
+	if len(visited) == len(graph): # Check for cycles
+		return visited
+	else:
+		return []
+
+
+
+
+
+
+'''
+graph = {'A': ['B', 'D'],
+         'B': ['C'],
+         'C': ['D', 'E'],
+         'D': ['E'],
+         'E': []}
+
+indegrees = {'A' : 0, 'B' : 1, 'C' : 1, 'D' : 2, 'E' : 2}
+
+print topsort(graph, indegrees)
+'''
+
+build_a_house = {'foundations' : ['walls'], 'walls' : ['roof', 'windows', 'plumbing'], 
+				  'roof' : ['decorating'], 'windows' : ['decorating'], 'plumbing' : ['decorating'],
+				   'decorating' : []}
+
+print topsort(build_a_house)
